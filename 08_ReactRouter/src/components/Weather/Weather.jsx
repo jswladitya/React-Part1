365903a8fcc666e08617581ea1618f455cf2f48d
jswladitya <jswladitya0403@gmail.com>
2024-09-20 15:15:04 +0530

    assets delete

--- 08_ReactRouter/src/components/Weather/Weather.jsx
+++ 08_ReactRouter/src/components/Weather/Weather.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import search_icon from '../Weather/assets/search.png'
 
 function Weather() {
   return (
     <div className='weather'>
       <div className='search-bar '>
         <input type="text" placeholder='search' />
-        <img src="" alt="" />
+        <img src={search_icon} alt="" />
       </div>
     </div>
   )
