e73d968828223400af10263290018c9477443c4d
jswladitya <jswladitya0403@gmail.com>
2024-11-20 22:38:38 +0530

    making custom react and updating codebase

--- 08_ReactRouter/src/components/Weather/Weather.jsx
+++ 08_ReactRouter/src/components/Weather/Weather.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import search_icon from '../Weather/assets/search.png'
+
 
 function Weather() {
   return (
-    <div className='weather'>
-      <div className='search-bar '>
+    <div className='weather place-self-center p-40 rounded-md bg-blue-700 flex flex-col items-center'>
+      <div className='search-bar  '>
         <input type="text" placeholder='search' />
-        <img src={search_icon} alt="" />
+        <img src="" alt="" />
+        {/* paste search img here */}
       </div>
     </div>
   )
